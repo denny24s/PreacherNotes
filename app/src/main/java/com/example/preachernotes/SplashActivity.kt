48ca4d6bbb80aced40a81ce93828103906c770f2
DaddyShark97 <shumikdenys@gmail.com>
2023-04-21 22:43:15 +0300

    SplashActivity

---
+++ app/src/main/java/com/example/preachernotes/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.preachernotes
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+
+    class SplashActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_splash)
+
+            supportActionBar?.hide()
+
+            val handler = Handler(Looper.getMainLooper())
+            handler.postDelayed({
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            },2000)
+        }
+    }
+}
